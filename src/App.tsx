06ad6d7c16c062ccdd944ec5f3b025beaff7c8ae
gapinska <gapinska.mon@gmail.com>
2021-06-22 23:11:49 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css"
 import EventComponent from "./events/EventComponent"
 import GuestList from "./state/GuestList"
 import UserSearch from "./state/UserSearch"
+import UserSearch1 from './state/UserSearch1'
 
 function App() {
   return (
     <div className="App">
-      <EventComponent/>
+      <UserSearch1/>
+      {/* <EventComponent/> */}
       {/* <UserSearch/> */}
       {/* <GuestList /> */}
     </div>
--- src/state/UserSearch1.tsx
+++ src/state/UserSearch1.tsx
@@ -1,23 +1,56 @@
 import { Component } from "react"
 
-interface UserSearchProps {
-  users: {
-    name: string
-    age: number
-  }[]
+const users=[
+  {name: 'Sarah', age: 20},
+  {name: 'Alex', age: 30},
+  {name: 'Michael', age: 40}
+]
+
+interface User {
+  name:string
+  age:number
 }
+
+// interface UserSearchProps {
+//   users: {
+//     name: string
+//     age: number
+//   }[]
+// }
+
+interface UserSearchProps {
+  users: User[]
+}
+
+// interface UserSearchState {
+//   name: string
+//   user: { name: string, age: number } | undefined 
+// }
 
 interface UserSearchState {
   name: string
-  user: { name: string | undefined }
+  user: User | undefined 
 }
-
 class UserSearch1 extends Component<UserSearchProps> {
-  state = {
+  state: UserSearchState = {
     name: "",
     user: undefined,
   }
-  render() {}
+
+  handleClick = ()=> {
+    const foundUser = users.find(user=>user.name===this.state.name)
+    this.setState({...this.state, user: foundUser})
+  }
+  render() {
+    const {user, name} = this.state
+    return(
+<div>User Search
+
+<input value={name} onChange={(e)=>this.setState({...this.state,name: e.target.value})}/>
+<button onClick={this.handleClick} >Find User</button>
+<div>{user?.name}</div>
+</div>)
+  }
 }
 
 export default UserSearch1
